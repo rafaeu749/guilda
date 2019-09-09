6dcdf71a77785fc8360f90aaa4a1844a5ed16c84
Rafaeu Silva <rafaeu749@hotmail.com>
2019-09-09 02:11:59 -0300

    Add more info to data.json

--- docs/js/guilda.js
+++ docs/js/guilda.js
@@ -1,5 +1,5 @@
 ﻿$().ready(function () {
-    $.getJSON("guilda/data/aventureiros.json", function (data) {
+    $.getJSON("/data/aventureiros.json", function (data) {
         console.log(data);
         $("#text").html(data["text"]);
     });
