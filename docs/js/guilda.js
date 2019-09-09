2422908489a670a2076609f894067bc82a848c9c
Rafaeu Silva <rafaeu749@hotmail.com>
2019-09-09 04:10:11 -0300

    Fix dynamic addition for data.json Added initial assets

--- docs/js/guilda.js
+++ docs/js/guilda.js
@@ -1,7 +1,43 @@
 ﻿$().ready(function () {
     $.getJSON("/guilda/data/aventureiros.json", function (data) {
-        console.log(data);
-        var jsData = JSON.parse(data);
-        $("#text").html(data["aventureiros"]);
+    //$.getJSON("/data/aventureiros.json", function (data) {
+        data["aventureiros"].forEach(showAventureiro);
     });
 });
+
+function getAventureiros() {
+
+}
+
+
+function showAventureiro(aventureiro) {
+    if (!aventureiro) return;
+    console.log(aventureiro);
+
+    $("#records").append(
+        $("<div>", {
+            "id": "test",
+            "class": "record rounded-lg shadow p-3 mb-5 bg-white rounded"
+        }).append(
+            $("<img>", {
+                class: "avatar rounded-lg",
+                src: aventureiro.imagem
+            })
+        ).append(
+            $("<div>", {
+                class: "row"
+            }).append(
+                $("<div>", {
+                    class: "col",
+                    html: "Nome: " + aventureiro.nome
+                })
+            ).append(
+                $("<div>", {
+                    class: "col",
+                    html: "Sexo: " + aventureiro.nome
+                })
+            )
+        )
+    );
+}
+
